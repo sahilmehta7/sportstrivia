00d782d4ecccde84692fe1327153b7c37f6403cb
sahilmehta7 <sahilmehta7@gmail.com>
2025-10-27 11:44:56 +0530

    Add bg-background to LayoutWrapper main element
    - Add bg-background class to the main container div
    - Add bg-background class to the main element
    - Ensures the pure black background is applied to all pages in dark mode
    - Fixes issue where main element was not showing black background

--- components/LayoutWrapper.tsx
+++ components/LayoutWrapper.tsx
@@ -16,9 +16,9 @@ export function LayoutWrapper({ children }: { children: React.ReactNode }) {
   }
 
   return (
-    <div className="flex min-h-screen flex-col">
+    <div className="flex min-h-screen flex-col bg-background">
       <MainNavigation />
-      <main className="flex-1">{children}</main>
+      <main className="flex-1 bg-background">{children}</main>
       <Footer />
     </div>
   );
